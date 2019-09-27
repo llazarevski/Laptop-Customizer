774fb9d7768f3281489159129ea6495f197358b1
Lindsey Lazarevski <lindsey.gillum@gmail.com>
2019-09-26 23:20:54 -0400

    Updating FeatureList and FeatureItem

--- src/FeatureItem/FeatureItem.js
+++ src/FeatureItem/FeatureItem.js
@@ -5,12 +5,27 @@ import './FeatureItem.css';
 class FeatureItem extends React.Component{
         render() {
           return (
+              <>
+            <div key={this.itemHash} className="feature__item">
+            <input
+              type="radio"
+              id={this.itemHash}
+              className="feature__option"
+              name={slugify(feature)}
+              checked={item.name === this.props.selected[feature].name}
+              onChange={e => this.updateFeature(feature, item)}
+            />
+            <label htmlFor={this.itemHash} className="feature__label">
+              {item.name} ({this.props.usCurrencyFormat.format(item.cost)})
+            </label>
+          </div>
             <fieldset className="feature" key={this.props.featureHash}>
                 <legend className="feature__name">
                  <h3>{this.props.feature}</h3>
                 </legend>
                 {this.props.options}
             </fieldset>
+            </>
             );
         };
 }
--- src/FeatureList/FeatureList.js
+++ src/FeatureList/FeatureList.js
@@ -10,25 +10,13 @@ class FeatureList extends React.Component{
           const options = this.props.features[feature].map(item => {
             const itemHash = slugify(JSON.stringify(item));
             return (
-              <div key={this.itemHash} className="feature__item">
-                <input
-                  type="radio"
-                  id={this.itemHash}
-                  className="feature__option"
-                  name={slugify(feature)}
-                  checked={item.name === this.props.selected[feature].name}
-                  onChange={e => this.updateFeature(feature, item)}
-                />
-                <label htmlFor={this.itemHash} className="feature__label">
-                  {item.name} ({this.props.usCurrencyFormat.format(item.cost)})
-                </label>
-                
-              </div>
+              <FeatureItem />
               
             );
           });
         });
-        return features;
+        return features
+        
 }
 }
 export default FeatureList;
