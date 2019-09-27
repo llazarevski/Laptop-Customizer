7a9426309741db8d3b1a6477e11930e9fb504a22
Lindsey Lazarevski <lindsey.gillum@gmail.com>
2019-09-26 22:49:19 -0400

    Cart is updated

--- src/CartListItem/CartListItem.js
+++ src/CartListItem/CartListItem.js
@@ -7,19 +7,20 @@ class CartListItem extends React.Component{
         const summary = Object.keys(this.props.selected).map((feature, idx) => {
         const featureHash = feature + '-' + idx;
         const selectedOption = this.props.selected[feature];
-        });
         return(
             <div className='CartListItem'>
                 <h2>Your cart</h2>
-                  <div className="summary__option" key={this.featureHash}>
+                  <div className="summary__option" key={featureHash}>
                     <div className="summary__option__label">{this.props.feature} </div>
-                    <div className="summary__option__value">{this.selectedOption.name}</div>
+                    <div className="summary__option__value">{selectedOption.name}</div>
                     <div className="summary__option__cost">
-                      {this.props.usCurrencyFormat.format(this.selectedOption.cost)}
+                      {this.props.usCurrencyFormat.format(selectedOption.cost)}
                     </div>
                   </div>
             </div>
                 );
+            });
+        return summary;
     };
 }
 
