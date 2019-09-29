0a443263c2beb337c35a97305b61543207a6d3ba
Lindsey Lazarevski <lindsey.gillum@gmail.com>
2019-09-29 14:31:09 -0400

    Correction Remove FeatureItem Component from FeatureItem

--- src/FeatureItem/FeatureItem.js
+++ src/FeatureItem/FeatureItem.js
@@ -6,27 +6,26 @@ import slugify from 'slugify';
 class FeatureItem extends React.Component{
         render() {
           return (
-              <>
-            <div key={this.props.itemHash} className="feature__item">
-            <input
-              type="radio"
-              id={this.props.itemHash}
-              className="feature__option"
-              name={slugify(this.props.feature)}
-              checked={this.props.item.name === this.props.selected[this.props.feature].name}
-              onChange={e => this.updateFeature(this.props.feature, this.props.item)}
-            />
-            <label htmlFor={this.props.itemHash} className="feature__label">
-              {this.props.item.name} ({this.props.usCurrencyFormat.format(this.props.item.cost)})
-            </label>
-            <FeatureItem key={this.props.itemHash}/>
-          </div>
-            <fieldset className="feature" key={this.props.featureHash}>
-                <legend className="feature__name">
-                 <h3>{this.props.feature}</h3>
-                </legend>
-                {this.props.options}
-            </fieldset>
+            <>
+                <div key={this.props.itemHash} className="feature__item">
+                <input
+                    type="radio"
+                    id={this.props.itemHash}
+                    className="feature__option"
+                    name={slugify(this.props.feature)}
+                    checked={this.props.item.name === this.props.selected[this.props.feature].name}
+                    onChange={e => this.updateFeature(this.props.feature, this.props.item)}
+                />
+                <label htmlFor={this.props.itemHash} className="feature__label">
+                    {this.props.item.name} ({this.props.usCurrencyFormat.format(this.props.item.cost)})
+                </label>    
+                </div>
+                <fieldset className="feature" key={this.props.featureHash}>
+                    <legend className="feature__name">
+                    <h3>{this.props.feature}</h3>
+                    </legend>
+                    {this.props.options}
+                </fieldset>
             </>
             
             );
--- src/FeatureList/FeatureList.js
+++ src/FeatureList/FeatureList.js
@@ -7,7 +7,7 @@ class FeatureList extends React.Component{
     render() {
         const features = Object.keys(this.props.features).map((feature, idx) => {
           const featureHash = feature + '-' + idx;
-          const options = this.props.features[this.props.feature].map(item => {
+          const options = this.props.features[feature].map(item => {
             const itemHash = slugify(JSON.stringify(item));
             return (
                 <FeatureItem key={itemHash}
