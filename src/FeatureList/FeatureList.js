c7a1adcd8ef43f2eec7fc0cdd7aabd07fb94f2bc
Lindsey Lazarevski <lindsey.gillum@gmail.com>
2019-09-29 15:19:05 -0400

    Test adding return statement to FeatureList

--- src/FeatureList/FeatureList.js
+++ src/FeatureList/FeatureList.js
@@ -10,16 +10,31 @@ class FeatureList extends React.Component{
           const options = this.props.features[feature].map(item => {
             const itemHash = slugify(JSON.stringify(item));
             return (
-                <FeatureItem key={itemHash}
-                options={options}
-                featureHash={featureHash}
-                itemHash ={itemHash}
-                item={item}/> 
+                <div key={itemHash} className="feature__item">
+                <input
+                    type="radio"
+                    id={itemHash}
+                    className="feature__option"
+                    name={slugify(feature)}
+                    checked={item.name === this.props.selected[feature].name}
+                    onChange={e => this.updateFeature(this.props.feature, this.props.item)}
+                />
+                <label htmlFor={itemHash} className="feature__label">
+                    {item.name} ({this.props.usCurrencyFormat.format(this.props.item.cost)})
+                </label>    
+                </div>
               
             );
           });
+          return (
+            <FeatureItem key={this.props.itemHash}
+            options={options}
+            featureHash={featureHash}
+            itemHash ={this.props.itemHash}
+            item={this.props.item}/>
+          )
         });
-       
+       console.log(features)
         return features
         
         
